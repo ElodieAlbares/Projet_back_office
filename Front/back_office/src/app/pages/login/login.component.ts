081e91896551899b5109078dc45260781e4ca220
elodie ALB <elodie.albares0201@gmail.com>
2025-03-10 20:40:01 +0100

    🔨 add gestion erreur login

--- Front/back_office/src/app/pages/login/login.component.ts
+++ Front/back_office/src/app/pages/login/login.component.ts
@@ -1,13 +1,13 @@
-// auth/login.component.ts
 import { Component } from '@angular/core';
 import { AuthService } from '../../core/service/auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone:true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
@@ -21,15 +21,12 @@ export class LoginComponent {
   onLogin() {
     this.authService.login(this.username, this.password).subscribe(
       (response) => {
-        // Enregistrer les tokens dans le localStorage
         localStorage.setItem('access_token', response.access);
         localStorage.setItem('refresh_token', response.refresh);
-        
-        // Rediriger vers la page d'accueil ou une page protégée
+
         this.router.navigate(['/home']);
       },
       (error) => {
-        // Gérer l'erreur (ex: identifiants invalides)
         this.errorMessage = 'Identifiants incorrects';
       }
     );
