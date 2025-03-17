1e700dfac60b5a16d835c676b8138c903bfbb058
elodie ALB <elodie.albares0201@gmail.com>
2025-03-17 15:55:18 +0100

    delete env projet

--- Back/backend_DBO_JWT/mySearchEngine/monTiGMagasin/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from monTiGMagasin import views
-
-urlpatterns = [
-    path('infoproducts/', views.InfoProductList.as_view()),
-    path('infoproduct/<int:tig_id>/', views.InfoProductDetail.as_view()),
-]
