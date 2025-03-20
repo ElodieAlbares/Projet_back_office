b3bcf5df1f1392af91e8f0e38c033d53720c20bf
Sofia <Sofia@BENARAIS>
2025-03-20 17:46:57 +0100

    dashboard historique ventes, composant front

--- Front/back_office/src/app/app.config.ts
+++ Front/back_office/src/app/app.config.ts
@@ -1,14 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
+  
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
      provideHttpClient()
      
- ]
+ ],
+
 };
--- Front/back_office/src/app/app.routes.ts
+++ Front/back_office/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+
+
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+    {path: 'history', component: HistoryComponent},
 ];
